d416ac4f51f0a1a40172ddfbe2be62a30fc0884b
Luis Alejo <lou.m.alejo@gmail.com>
2020-06-12 15:54:43 -0400

    basic version complete

--- App/index.js
+++ App/index.js
@@ -1,20 +1,176 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TouchableOpacity, Dimensions, Picker } from 'react-native';
+
+const screen = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+	container: {
+		flex: 1,
+		backgroundColor: '#07121B',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	button: {
+		borderWidth: 10,
+		borderColor: '#89AAFF',
+		width: screen.width / 2,
+		height: screen.width / 2,
+		borderRadius: screen.width / 2,
+		alignItems: 'center',
+		justifyContent: 'center',
+		marginTop: 30
+	},
+	buttonStop: {
+		borderColor: '#ff851B'
+	},
+	buttonText: {
+		fontSize: 45,
+		color: '#89AAFF'
+	},
+	buttonTextStop: {
+		color: '#ff851b'
+	},
+	timerText: {
+		color: '#fff',
+		fontSize: 90
+	},
+	picker: {
+		width: 50
+	},
+	pickerItem: {
+		color: '#fff',
+		fontSize: 20
   },
+  pickerContainer: {
+    flexDirection: "row", 
+    alignItems: 'center'
+  }
 });
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+const formatNumber = (number) => `0${number}`.slice(-2);
 
+const getRemaining = (time) => {
+	const minutes = Math.floor(time / 60);
+	const seconds = time - minutes * 60;
+	return { minutes: formatNumber(minutes), seconds: formatNumber(seconds) };
+};
+
+const createArray = (length) => {
+	const arr = [];
+	let i = 0;
+	while (i < length) {
+		arr.push(i.toString());
+		i += 1;
+	}
+	return arr;
+};
+
+const AVAILABLE_MINUTES = createArray(60);
+const AVAILABLE_SECONDS = createArray(60);
+
+export default class App extends React.Component {
+	state = {
+		remainingSeconds: 0,
+    isRunning: false, 
+    selectedMinutes: "0", 
+    selectedSeconds: "0"
+	};
+
+	interval = null;
+
+	start = () => {
+		this.setState((state) => ({
+			remainingSeconds: state.remainingSeconds - 1
+		}));
+	};
+
+	componentDidUpdate(prevProp, prevState) {
+		if (this.state.remainingSeconds === 0 && prevState.remainingSeconds !== 0) {
+			this.stop();
+		}
+	}
+
+	componentWillUnmount() {
+		if (this.interval) {
+			clearInterval(this.interval);
+		}
+	}
+
+	start = () => {
+		this.setState((state) => ({
+			remainingSeconds: parseInt(state.selectedMinutes, 10) * 60 + parseInt(state.selectedSeconds, 10),
+			isRunning: true
+		}));
+
+		this.interval = setInterval(() => {
+			this.setState((state) => ({
+				remainingSeconds: state.remainingSeconds - 1
+			}));
+		}, 1000);
+	};
+
+	stop = () => {
+		clearInterval(this.interval);
+		this.interval = null;
+		this.setState({
+			remainingSeconds: 0,
+			isRunning: false
+		});
+  };
+
+	renderPickers = () => {
+		return (
+			<View style={styles.pickerContainer}>
+				<Picker
+					style={styles.picker}
+					itemStyle={styles.pickerItem}
+					selectedValue={this.state.selectedMinutes}
+					onValueChange={(itemValue) => {
+						this.setState({
+              selectedMinutes: itemValue
+            })
+					}}
+				>
+					{AVAILABLE_MINUTES.map((value) => <Picker.Item key={value} label={value} value={value} />)}
+				</Picker>
+        <Text style={styles.pickerItem}>minutes</Text>
+				<Picker
+					style={styles.picker}
+					itemStyle={styles.pickerItem}
+					selectedValue={this.state.selectedSeconds}
+					onValueChange={(itemValue) => {
+						this.setState({
+              selectedSeconds: itemValue
+            })
+					}}
+				>
+					{AVAILABLE_SECONDS.map((value) => <Picker.Item key={value} label={value} value={value} />)}
+				</Picker>
+        <Text style={styles.pickerItem}>seconds</Text>
+			</View>
+		);
+	};
+
+	render() {
+		const { minutes, seconds } = getRemaining(this.state.remainingSeconds);
+		return (
+			<View style={styles.container}>
+				<StatusBar barStyle="light-content" />
+				{this.state.isRunning ? (
+					<Text style={styles.timerText}>{`${minutes}: ${seconds}`}</Text>
+				) : (
+					this.renderPickers()
+				)}
+				{!this.state.isRunning ? (
+					<TouchableOpacity onPress={this.start} style={styles.button}>
+						<Text style={styles.buttonText}>Start</Text>
+					</TouchableOpacity>
+				) : (
+					<TouchableOpacity onPress={this.stop} style={[ styles.button, styles.buttonStop ]}>
+						<Text style={[ styles.buttonText, styles.buttonTextStop ]}>Stop</Text>
+					</TouchableOpacity>
+				)}
+			</View>
+		);
+	}
+}
